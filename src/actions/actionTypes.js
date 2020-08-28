8d9d1e15a3836051b5366c6a211eb5235d93f027
Comfort <65386761+cnorteye@users.noreply.github.com>
2020-08-27 18:47:12 -0700

    started on redux w actions and reducers

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export const SEARCH_JOB = 'SEARCH_JOB';
+export const FETCH_JOBS = 'FETCH_JOBS';
+export const FETCH_JOB = 'FETCH_JOB';
+export const LOADING = 'LOADING';
---
+++ src/actions/searchActions.js
@@ -0,0 +1,39 @@
+import { SEARCH_JOB, FETCH_JOBS, FETCH_JOB, LOADING } from './types';
+import axios from 'axios';
+
+export const searchJob = text => dispatch => {
+  dispatch({
+    type: SEARCH_JOB,
+    payload: text
+  });
+};
+
+export const fetchJobs = (description, location, fulltime) => dispatch => {
+  axios
+    .get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${description}&full_time=${fulltime}&location=${location}`)
+    .then(response =>
+      dispatch({
+        type: FETCH_JOBS,
+        payload: response.data
+      })
+    )
+    .catch(err => console.log(err));
+};
+
+export const fetchJob = id => dispatch => {
+  axios
+    .get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions/${id}.json?`)
+    .then(response =>
+      dispatch({
+        type: FETCH_JOB,
+        payload: response.data
+      })
+    )
+    .catch(err => console.log(err));
+};
+
+export const setLoading = () => {
+  return {
+    type: LOADING
+  };
+};
